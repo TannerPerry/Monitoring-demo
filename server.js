23857e7e0ecab8c65d38002fe93b764a419ada04
Tanner Perry <tc-boy@hotmail.com>
2021-10-26 11:20:00 -0600

    ready for hosting

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path')
 
 const app = express()
 
-app.get('/', (req, res)=> {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
 const port = process.env.PORT || 4545
 
-app.listen(4545, () => console.log(`Take us to warp ${port}!`))
+app.listen(port, () => console.log(`Take us to warp ${port}!`))
