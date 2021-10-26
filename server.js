60baa6c4237f12686dc5573e7682f14c1b0d6fb3
Tanner Perry <tc-boy@hotmail.com>
2021-10-26 12:38:01 -0600

    dynamic error handling

--- server.js
+++ server.js
@@ -17,16 +17,29 @@ app.get('/', (req, res) => {
     rollbar.info('html file served successfully')
 })
 
+app.get('/style', (req, res) =>{
+    res.sendFile(path.join(__dirname, '/public/styles.css'))
+})
 
 let students = []
 
 app.post('/api/student', (req,res ) =>{
     let {name} = req.body
     name = name.trim()
-    students.push(name)
-    rollbar.log('student added successfully', {author: 'Tanner', type: 'manual entry'})
-    res.status(200).send(students)
+   
+    const index = students.findIndex(studentName => studentName === name)
 
+    if (index === -1 && name !== '') {
+        students.push(name)
+        rollbar.log('student added successfully', {author: 'Tanner', type: 'manual entry'})
+        res.status(200).send(students)    
+    } else if (name === '') {
+    rollbar.error('no name given')
+    res.status(400).send('must provide a name')
+    } else {
+        rollbar.critical('name has already be added')
+        res.status(400).send('that student already exists')
+    }
 })
 
 app.use(rollbar.errorHandler())
